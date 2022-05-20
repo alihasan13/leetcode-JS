005f0e5847f31a1728fbd2a7a85f07c306e677e6
Ali Hasan <alihasan13@users.noreply.github.com>
2022-05-20 23:56:34 +0600

    prb 9 solved using js

---
+++ PalindromeNumberI(leetcode#9).js
@@ -0,0 +1,13 @@
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }
+    let number = x;
+    // This will store the reverse of the number
+    let reverse = 0;
+    while (number > 0) {
+        reverse = reverse * 10 + number % 10;
+        number = parseInt(number / 10);
+    }
+    return x === reverse;
+};
