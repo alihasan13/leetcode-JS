fedd6c71d010233257d5f5e76879dbc14e40004e
Ali Hasan <alihasan13@users.noreply.github.com>
2023-01-01 23:18:13 +0600

    prb 1480 modified

--- Running Sum of 1d Array(leetcode#1480).js
+++ Running Sum of 1d Array(leetcode#1480).js
@@ -1,9 +1,14 @@
 var runningSum = function(nums) {
-    let ans = new Array(nums.length)
-    ans[0] = nums[0]
-    for (let i = 1; i < nums.length; i++)
-        ans[i] = ans[i-1] + nums[i]
-    return ans
+    let sum = 0 ,_temp = [];
+
+  for ( let i=0; i< nums.length;i++){
+      
+      sum = sum + nums[i]
+      
+      _temp.push(sum)
+      
+  }
+    return _temp;
 };
 nums =[1,2,3,4]
 runningSum(nums)
