4d3746f118c0a0d215031dd51a81eadf980161c1
Ali Hasan <alihasan13@users.noreply.github.com>
2023-01-02 23:58:07 +0600

    updated

--- PivotIndex(leetcode#724).js
+++ PivotIndex(leetcode#724).js
@@ -1,20 +1,14 @@
-function pivotIndex(nums) {
-    let totalSum = 0
-    for(let i = 0; i < nums.length; i++) {
-        totalSum += nums[i]
-    }
-    
-    let leftSum = 0
-    
-    for (let i = 0; i < nums.length; i++) {
-        let rightSum = totalSum - nums[i] - leftSum
-        if (rightSum === leftSum) {
-            return i
-        }
-        leftSum += nums[i]
-    }
-    return -1
-};
+var pivotIndex = function(nums) {
+    let leftPivot = 0, rightPivot = 0, sum =0;
+    sum= nums.reduce((a,b)=>a+b)
+      
+     for( let i=0 ; i <nums.length; i++){
+         rightPivot = sum - nums[i]- leftPivot
+         if(rightPivot==leftPivot) return i
+         leftPivot = leftPivot+nums[i]
+     }
+     return -1
+ };
 
 
 console.log(pivotIndex([2,3,4,5]))
