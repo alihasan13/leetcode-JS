13d93cdfd15f7812b2528f902cc1a116ed8e3f5a
alihasan13 <ali.hasan@northsouth.edu>
2023-01-16 23:59:26 +0600

    solved 20

---
+++ Valid Parentheses(leetcode#20).js
@@ -0,0 +1,15 @@
+var isValid = function(s) {
+    let expectedBrackets = [];
+
+    for (let letter = 0; letter < s.length; letter++) {
+        if (s[letter] == '{') {
+            expectedBrackets.push('}');
+        } else if (s[letter] == '[') {
+            expectedBrackets.push(']');
+        } else if (s[letter] == '(') {
+            expectedBrackets.push(')');
+                 }
+        else if (expectedBrackets.pop() !== s[letter]) {
+            return false;
+        }
+    }
