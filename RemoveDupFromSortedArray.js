6c3c7a61f5cd4b695a42c9dc6c171bebbd32e695
Ali Hasan <alihasan13@users.noreply.github.com>
2023-02-14 23:58:58 +0600

    solution added

---
+++ RemoveDupFromSortedArray.js
@@ -0,0 +1,11 @@
+var removeDuplicates = function(nums) {
+    let index = 1;
+    for ( let i=1 ; i<nums.length; i++){
+        if ( nums[i-1]!=nums[i]){
+           nums[index]= nums[i];
+            index++;
+        }
+        
+    }
+    return index;
+};
