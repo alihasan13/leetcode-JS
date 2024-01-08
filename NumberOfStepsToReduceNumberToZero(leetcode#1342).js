f011530001d165cbb4854a3f7151704c377ccfad
Ali Hasan <alihasan13@users.noreply.github.com>
2024-01-08 23:41:06 +0600

    solved leetcode 1342

---
+++ NumberOfStepsToReduceNumberToZero(leetcode#1342).js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let counter = 0
+    
+    while(num !=0){
+       if(num%2==0) num =num/2
+    else if(num%2 !=0) num = num -1
+        counter = counter +1
+       }
+    return counter;
+};
